e9f2d2e15e629775a60b766f2b868a497fb70224
Yingming <ustcsilence@gmail.com>
2022-06-10 21:19:14 +0800

    First commit
    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 21:04:06 2022
+
+@author: ustcs
+"""
+
+
+
+
+
+if __name__ == '__main__':
+    pass
---
+++ test_gui.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+
+
+if __name__ == '__main__':
+    """
+    Test some functions of "pyautogui"
+    """
+    # My screen resolution
+    print(pyautogui.size()) # Size(width=2560, height=1440)
+    
